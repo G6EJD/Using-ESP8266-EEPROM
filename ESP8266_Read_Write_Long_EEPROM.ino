32a0b4547263ec4fad44203ad5abcabafa33d3a2
Naman Chauhan <29328764+chauhannaman98@users.noreply.github.com>
2019-07-12 12:38:40 +0530

    Update ESP8266_Read_Write_Long_EEPROM.ino

--- ESP8266_Read_Write_Long_EEPROM.ino
+++ ESP8266_Read_Write_Long_EEPROM.ino
@@ -6,7 +6,7 @@ const long value = 2546824;
 void setup() {
 
     Serial.begin(9600);
-    EEPROM.begin(32);       // Initialising array of size 32 bytes in RAM to represent the 32 bytes in Flash as EEPROM
+    EEPROM.begin(32);       // Initialising array of size 32 bytes in RAM to simulate the 32 bytes in Flash as EEPROM
 
     //writing
     EEPROMWrite(EEaddress, value);
@@ -29,8 +29,8 @@ void loop() {
 //function to write to EEPROM
 void EEPROMWrite(int address, long value) {
 
-    /* size of long is 32 bytes but each memory location in EEPROM
-     * takes only 8 bytes and then rolls over. Thus, it is taken into 4 individual bytes
+    /* size of long is 32-bits but each memory location in EEPROM
+     * takes only 8-bits and then rolls over. Thus, it is taken into 4 individual bytes
      * and then stored in consecutive memory locations.
      */
     byte four = (value & 0xFF);
@@ -51,18 +51,19 @@ long EEPROMRead(long address) {
     /* Reading individual bytes here from each memory location into 
      * different variables.
      */
-    long four = EEPROM.get(address);
-    long three = EEPROM.get(address + 1);
-    long two = EEPROM.get(address + 2);
-    long one = EEPROM.get(address + 3);
+    byte four = EEPROM.get(address);       // least LSB
+    byte three = EEPROM.get(address + 1);       
+    byte two = EEPROM.get(address + 2);
+    byte one = EEPROM.get(address + 3);     // most MSB
 
     return (
           /*Combining all the four read outputs for retrieving the saved value from EEPROM
           * It's reverse procedure of what is being done in EEPROMWrite().
+          * Here, long is stored as big endian 
           */
-          ((four << 0) & 0xFF) + 
+          ((four << 0) & 0xFF) +        // retreiving least LSB
           ((three << 8) & 0xFFFF) + 
           ((two << 16) & 0xFFFFFF) + 
-          ((one << 24) & 0xFFFFFFFF)
+          ((one << 24) & 0xFFFFFFFF)    // retreiving most MSB
       );
 }
